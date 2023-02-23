32d6bca6829110a7c8c0c07e4f38c573f8b65856
iwetta <iwetta.majewska@gmail.com>
2023-02-23 21:43:43 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
-  return <p>test test</p>;
+  return <FirstComponent />;
 }
 
 export default App;
---
+++ src/components/FirstComponent.js
@@ -0,0 +1,5 @@
+const FirstComponent = () => {
+  return <p>my first component</p>;
+};
+
+export default FirstComponent;
